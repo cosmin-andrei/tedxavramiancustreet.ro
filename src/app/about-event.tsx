b0f707817bbd0f80858abd436b486988139d447f
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-12-13 22:39:02 +0200

    fix error

--- src/app/about-event.tsx
+++ src/app/about-event.tsx
@@ -21,21 +21,38 @@ const EVENT_INFO = [
 export function AboutEvent() {
   return (
     <section className="container mx-auto flex flex-col items-center px-4 py-10">
-      <Typography variant="h6" className="text-center mb-2" color="orange">
-        About the event
-      </Typography>
-      <Typography variant="h3" className="text-center" color="blue-gray">
-        Why Attend?
-      </Typography>
-      <Typography
-        variant="lead"
-        className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-gray-500"
-      >
-        Welcome to the AI Conference 2023, where the future unfolds! Whether
-        you&apos;re a seasoned AI professional, a curious newcomer, or a
-        business leader looking to harness the power of AI, this conference is
-        designed to inspire, educate, and connect.
-      </Typography>
+        <Typography
+            variant="h6"
+            className="text-center mb-2"
+            color="orange"
+            placeholder=""
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
+        >
+            About the event
+        </Typography>
+        <Typography
+            variant="h3"
+            className="text-center"
+            color="blue-gray"
+            placeholder=""
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
+        >
+            Why Attend?
+        </Typography>
+        <Typography
+            variant="lead"
+            className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-gray-500"
+            placeholder=""
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
+        >
+            Welcome to the AI Conference 2023, where the future unfolds! Whether
+            you&apos;re a seasoned AI professional, a curious newcomer, or a
+            business leader looking to harness the power of AI, this conference is
+            designed to inspire, educate, and connect.
+        </Typography>
       <div className="mt-8 w-full grid grid-cols-1 md:grid-cols-2 gap-4 ">
         {EVENT_INFO.map((props, idx) => (
           <AboutCard key={idx} {...props} />
