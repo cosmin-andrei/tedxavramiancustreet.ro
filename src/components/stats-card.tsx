15591ad15914cc5a22f9baab64e6c1556f5699fb
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-12-13 23:44:31 +0200

    fix error

--- src/components/stats-card.tsx
+++ src/components/stats-card.tsx
@@ -1,22 +1,41 @@
-
 import { Typography, Card } from "@material-tailwind/react";
+
 interface StatsCardProps {
-  count: string;
-  title: string;
+    count: string;
+    title: string;
 }
 
 export function StatsCard({ count, title }: StatsCardProps) {
-  return (
-    <Card color="transparent" shadow={false}>
-      <Typography variant="h1" className="font-bold" color="blue-gray">
-        {count}
-      </Typography>
-      <Typography variant="h6" color="blue-gray" className="mt-1 font-medium">
-        {title}
-      </Typography>
-    </Card>
-  );
+    return (
+        <Card
+            color="transparent"
+            shadow={false}
+            placeholder=""
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
+        >
+            <Typography
+                variant="h1"
+                className="font-bold"
+                color="blue-gray"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
+            >
+                {count}
+            </Typography>
+            <Typography
+                variant="h6"
+                color="blue-gray"
+                className="mt-1 font-medium"
+                placeholder=""
+                onPointerEnterCapture={() => {}}
+                onPointerLeaveCapture={() => {}}
+            >
+                {title}
+            </Typography>
+        </Card>
+    );
 }
-
 
 export default StatsCard;
