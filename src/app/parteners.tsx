e51f7f4231e15524ce7c3b31586a785b5a6b4f43
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-12-16 11:21:32 +0200

    fix err

--- src/app/parteners.tsx
+++ src/app/parteners.tsx
@@ -1,58 +1,53 @@
 "use client";
 
-import React, { Suspense } from "react";
+import React, {Suspense} from "react";
 import Image from "next/image";
-import { Typography } from "@material-tailwind/react";
+import {Typography} from "@material-tailwind/react";
 
 const SPONSORS = [
-  "onedu.png",
-  "epg.png",
-  "aqua.svg",
-  "jidvei.png",
-  "avon.png",
-  "cartofiserie.png",
-  "publica.webp",
-  "m5.webp",
-  "cinema.webp",
-  "primarie.webp",
-  "ellproject.png"
+    "onedu.png",
+    "epg.png",
+    "aqua.svg",
+    "jidvei.png",
+    "avon.png",
+    "cartofiserie.png",
+    "publica.webp",
+    "m5.webp",
+    "cinema.webp",
+    "primarie.webp",
+    "ellproject.png"
 ];
 
 export function Parteners() {
-  return (
-      <section className="py-8 px-8 lg:py-20" id="parteneri">
-        <div className="missionContainer mx-auto text-center">
-          <Typography
-              variant="h3"
-              className="text-black text-center font-bold mb-6"
-              as="h3"
-              placeholder=""
-              onPointerEnterCapture={() => {
-              }}
-              onPointerLeaveCapture={() => {
-              }}
-          >
-           Partenerii ediției 2024
-          </Typography>
-          <div className="flex flex-wrap items-center justify-center gap-6">
-            {SPONSORS.map((logo, key) => (
-                <div
-                    key={key}
-                    className="w-40 h-40 flex items-center justify-center bg-white border border-gray-300 rounded-lg p-4"
+    return (
+        <section className="py-8 px-8 lg:py-20" id="parteneri">
+            <div className="missionContainer mx-auto text-center">
+                <Typography
+                    variant="h3"
+                    className="text-black text-center font-bold mb-6"
+                    as="h3"
                 >
-                  <Image
-                      width={156}
-                      height={156}
-                      src={`/image/editia2024/parteners/logo_${logo}`}
-                      alt={logo}
-                      className="object-contain"
-                  />
+                    Partenerii ediției 2024
+                </Typography>
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 items-center justify-center">
+                    {SPONSORS.map((logo, key) => (
+                        <div
+                            key={key}
+                            className="flex items-center justify-center bg-white border border-gray-300 rounded-lg p-4"
+                        >
+                            <Image
+                                width={156}
+                                height={156}
+                                src={`/image/editia2024/parteners/logo_${logo}`}
+                                alt={logo}
+                                className="object-contain"
+                            />
+                        </div>
+                    ))}
                 </div>
-            ))}
-          </div>
-        </div>
-      </section>
-  );
+            </div>
+        </section>
+    );
 }
 
 export default Parteners;
